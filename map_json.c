538ee73137a850e21d14ca1f16fc301f9c989930
Abialbon Paul <abialbon@Abialbons-MacBook-Air.local>
2019-02-25 20:29:07 -0400

    corrected map_json

--- map_json.c
+++ map_json.c
@@ -6,10 +6,20 @@ int main(void)
     float longitude;
     char info[80];
     int started = 0;
-    printf("{\n");
+    printf("[\n");
     while(scanf("%f,%f,%79[^\n]", &latitude, &longitude, info) == 3)
     {
-        if (started)
+        if (latitude < -90 || latitude > 90)
+        {
+            fprintf(stderr, "Invalid latitude: %f\n", latitude);
+            return 2;
+        }
+        else if (longitude < -180 || longitude > 180)
+        {
+            fprintf(stderr, "Invalid longitude: %f\n", longitude);
+            return 2;
+        }
+        else if (started)
         {
             printf(",\n");
         }
@@ -17,7 +27,7 @@ int main(void)
         {
             started = 1;
         }
-        printf("latitude: %f, longitude: %f, info: '%s'", latitude, longitude, info);
+        printf("{\"latitude\": %f, \"longitude\": %f, \"info\": \"%s\"}", latitude, longitude, info);
     }
-    printf("\n}\n");
+    printf("\n]\n");
 }
