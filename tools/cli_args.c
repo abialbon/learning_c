521908de775f711cc354999a5c0ab88b3652d212
Abialbon Paul <dr.abialbon@gmail.com>
2019-03-25 21:33:20 -0400

    comments_test

--- tools/cli_args.c
+++ tools/cli_args.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// The argc is the number of arguments that is passed into the main function
+// the argv is a string array of all the arguments that are passed
 int main(int argc, char *argv[])
 {
     for (int i = 1; i < argc; i++)
